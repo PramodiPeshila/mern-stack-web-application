e35ebaf7d58b5125d72fff0b2769f8d29da6789f
Pramodi Peshila <ppeshila7@gmail.com>
2024-08-16 07:06:18 +0530

    created new component  'FoodItem'

--- src/components/FoodDisplay/foodDisplay.jsx
+++ src/components/FoodDisplay/foodDisplay.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import './foodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
 
 
@@ -11,6 +12,11 @@ const FoodDisplay = ({category}) => {
   return (
     <div className='food-display' id='food-display'>
         <h2>Top Dishes near you</h2>
+        <div className="food-display-list">
+          {food_list.map((item,index)=>{
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+          })}
+        </div>
     </div>
   )
 }
